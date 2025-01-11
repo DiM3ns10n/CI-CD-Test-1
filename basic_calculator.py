0a694fc7c629d936ebd52b0aa0f66e364455ffe6
Mayank Mahadik <mayank.mahadik@gmail.com>
2025-01-11 18:43:01 +0530

    added basic_calculator.py

---
+++ basic_calculator.py
@@ -0,0 +1,30 @@
+class BasicCalculator():
+    def __init__(self, *args):
+        self.arguments = args
+    
+    def __repr__(self):
+        return f"BasicCalculator({self.arguments})"
+
+    def add(self):
+        return sum(self.arguments)
+    
+    def subtract(self):
+        subtract = self.arguments[0] - sum(self.arguments[1:])
+        return subtract
+    
+    def multiply(self):
+        multiply = 1
+        for i in self.arguments:
+            multiply *= i
+        return multiply
+    
+    def divide(self):
+        divide = self.arguments[0]
+        for i in self.arguments[1:]:
+            divide /= i
+        return divide
+    
+
+    
+    
+    
